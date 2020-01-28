69da0be32d5c134bde25650ced8b211215443e0e
Albert Tokarski <tokarskialbert@gmail.com>
2020-01-28 20:55:28 +0100

    PoemBeautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("PERSON", text -> text.toLowerCase());
+        poemBeautifier.beautify("PERSON", text -> text.concat(" - HUMAN"));
+        poemBeautifier.beautify("PERSON", text -> text.replace('E','3'));
+
+
+    }
+}
