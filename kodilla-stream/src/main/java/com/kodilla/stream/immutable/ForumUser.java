ff639c41deef38434843805fc3cf723f043232d7
Albert Tokarski <tokarskialbert@gmail.com>
2020-01-29 22:02:06 +0100

    ForumUser class  modified

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,12 +1,20 @@
 package com.kodilla.stream.immutable;
 
+import com.sun.tools.javac.util.List;
+
+import java.util.Date;
+
 public final class ForumUser {
     private final String username;
     private final String realName;
+    private final List<ForumUser> friends;
+    private final Date birthDate;
 
-    public ForumUser(final String username, final String realName) {
+    public ForumUser(String username, String realName, List<ForumUser> friends, Date birthDate) {
         this.username = username;
         this.realName = realName;
+        this.friends = friends;
+        this.birthDate = birthDate;
     }
 
     public String getUsername() {
@@ -15,5 +23,13 @@ public final class ForumUser {
 
     public String getRealName() {
         return realName;
+    }
+
+    public List<ForumUser> getFriends() {
+        return friends;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
     }
 }
