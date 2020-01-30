33193429333db296dd100aa63559d33c8a4e7898
Albert Tokarski <tokarskialbert@gmail.com>
2020-01-30 21:07:06 +0100

    PoemBeautifier changed

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -3,7 +3,6 @@ package com.kodilla.stream.beautifier;
 public class PoemBeautifier {
     public String beautify(String text, PoemDecorator poemDecorator) {
        String myText = poemDecorator.decorate(text);
-       System.out.println(myText);
 
        return myText;
     }
