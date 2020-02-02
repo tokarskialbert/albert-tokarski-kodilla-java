576633888a437300a2b8c8e15232839bb417d7b0
Albert Tokarski <tokarskialbert@gmail.com>
2020-02-02 18:48:16 +0100

    srednia ilosc dni realizacji zadania

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -10,12 +10,5 @@ import java.util.stream.Collectors;
 public class StreamMain {
     public static void main(String[] args) {
 
-        Forum forum = new Forum();
-
-        Map theResultMapOfForumUsers = forum.getForumUserList().stream()
-                .filter(forumUser -> forumUser.getNumberOfUserPosts() > 0)
-                .filter(forumUser -> Character.toString(forumUser.getSex()).equals("M"))
-                .filter(forumUser -> Period.between(forumUser.getBirthDate(), LocalDate.now()).getYears() > 19)
-                .collect(Collectors.toMap(forumUser -> forumUser.getUserID(), forumUser -> forumUser));
     }
 }
